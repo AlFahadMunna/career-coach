8521b92710f5002c64fd11934be4a911ceb7cc17
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-07 23:30:43 +0600

    career caoch project setup completed

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,21 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* header */}
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made with 💗 by AFM</p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
