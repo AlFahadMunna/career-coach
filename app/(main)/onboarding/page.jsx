b0ea9d0cecbf04b3edea71d157fc158e1ba95b2a
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-16 11:16:57 +0600

    some pages added

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,7 +1,13 @@
+import { industries } from "@/data/industries";
 import React from "react";
+import OnboardingForm from "./_components/onboarding-form";
 
 const OnboardingPage = () => {
-  return <div>OnboardingPage</div>;
+  return (
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
+  );
 };
 
 export default OnboardingPage;
