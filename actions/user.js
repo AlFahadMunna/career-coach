571a516ae2f9a4d83fca2015a0f7a7893b716604
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-18 10:35:26 +0600

    actions and some changes added

--- actions/user.js
+++ actions/user.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "@/lib/prisma";
-import { auth } from "@clerk/nextjs /server";
+import { auth } from "@clerk/nextjs/server";
 
 export async function updateUser(data) {
   const { userId } = await auth();
--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { onboardingSchema } from "@/app/lib/schema";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 
 const OnboardingForm = ({ industries }) => {
-  return <div>OnboardingForm</div>;
+  const [selectedIndustry, setSelectedIndustry] = useState(null);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm({
+    resolver: zodResolver(onboardingSchema),
+  });
+
+  return (
+    <div className="flex items-center justify-center bg-background">
+      <Card className="w-full max-w-lg mt-10 mx-2">
+        <CardHeader>
+          <CardTitle className="gradient-title text-4xl">
+            Complete Your Profile
+          </CardTitle>
+          <CardDescription>
+            Select your industry to ger personalized career insights and
+            recommendations.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div>
+              <Label htmlFor="industry">Industry</Label>
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default OnboardingForm;
